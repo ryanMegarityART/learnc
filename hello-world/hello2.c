6f007d339a60fbab7b7cd6bebd25836eeac782ad
ryan <ryanmegarity@gmail.com>
2023-05-19 12:43:34 +0100

    add data types lesson

---
+++ hello-world/hello2.c
@@ -0,0 +1,15 @@
+/*
+ * hello2.c
+ * My first C program with comments
+ * by ryan megarity
+ * created 19-05-2023
+ */
+
+#include <stdio.h>
+
+int main() { 
+	printf("Hello World 2\n");
+	return 0;
+}
+
+/*eof*/
